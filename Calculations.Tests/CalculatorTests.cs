ac6db9b2c0d54149e1879d91400b64b42a9054fe
Nasrudin Toefy <NasrudinT@korbicom.com>
2021-05-05 14:05:33 +0200

    Add project files.

---
+++ Calculations.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Calculations2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Calculations2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
