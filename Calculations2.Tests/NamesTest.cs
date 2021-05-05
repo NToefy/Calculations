4a5b5dd46927f204bdef7e9d69f29923eb7991b3
Nasrudin Toefy <NasrudinT@korbicom.com>
2021-05-05 14:37:25 +0200

    Assert with Strings

---
+++ Calculations2.Tests/NamesTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Calculations;
+
+namespace Calculations2.Tests
+{
+    public class NamesTest
+    {
+        [Fact]
+
+        public void MakeFullName_GivenString_ReturnEqual()
+        {
+            var names = new Names();
+            var result = names.MakeFullName("Joe", "Black");
+            Assert.Equal("Joe Black", result);
+        }
+    }
+}
