22821018306fa7ba09307e83972e13f90c5704e5
Nasrudin Toefy <NasrudinT@korbicom.com>
2021-05-05 14:18:24 +0200

    First Unit Tests added

--- Calculations2.Tests/UnitTest1.cs
+++ Calculations2.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Calculations;
 using System;
 using Xunit;
 
@@ -6,9 +7,19 @@ namespace Calculations2.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Add_GivenTwoIntValues_ReturnsInt()
         {
-            Assert.True(true);
+            var calc = new Calculator();
+            var result = calc.Add(1,2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void AddDouble_GivenTwoDoubleValues_ReturnsDouble()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.2, 3.5);
+            Assert.Equal(4.7, result);
         }
     }
 }
