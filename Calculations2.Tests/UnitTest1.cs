e8a183f76ea1eafd67286b5beb2a65c3fb1b4c4e
Nasrudin Toefy <NasrudinT@korbicom.com>
2021-05-06 07:58:28 +0200

    Assert Collections

--- Calculations2.Tests/UnitTest1.cs
+++ Calculations2.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Calculations;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Calculations2.Tests
@@ -20,6 +21,35 @@ namespace Calculations2.Tests
             var calc = new Calculator();
             var result = calc.AddDouble(1.2, 3.5);
             Assert.Equal(4.7, result);
+        }
+
+        [Fact]
+        public void FiboDoesNotIncludeZero()
+        {
+            var calc = new Calculator();
+            Assert.All(calc.FiboNumbers, n => Assert.NotEqual(0, n));
+        }
+
+        [Fact]
+        public void FiboIncludes()
+        {
+            var calc = new Calculator();
+            Assert.Contains(13, calc.FiboNumbers);
+        }
+
+        [Fact]
+        public void FiboNotInclude4()
+        {
+            var calc = new Calculator();
+            Assert.DoesNotContain(4, calc.FiboNumbers);
+        }
+
+        [Fact]
+        public void CheckFiboCollection()
+        {
+            var expectedCollection = new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+            var calc = new Calculator();
+            Assert.Equal(expectedCollection, calc.FiboNumbers);
         }
     }
 }
