1e17ed96e620c0babbd9d028c2e20f3abf5e3953
Nasrudin Toefy <NasrudinT@korbicom.com>
2021-05-06 08:14:45 +0200

    WIP Assert Range

---
+++ Calculations2.Tests/CustomerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Calculations;
+
+namespace Calculations2.Tests
+{
+    public class CustomerTest
+    {
+    }
+}
